4bfabdcc667f9dd6631e508e648b12f5f068aeae
crspr-cas9 <82477114+crspr-cas9@users.noreply.github.com>
2021-04-13 23:58:50 +0200

    Commit message ?

---
+++ src/main/java/Firstrun.java
@@ -0,0 +1,18 @@
+public class Firstrun{
+    public static void main(String[] args){
+        //alright lets Hope and pray
+        System.out.println("Hello,World.");
+        //By the power of jesus christ
+        System.out.print("This is the beginning of something awesome.");
+        System.out.println(".....Mark my codes.");
+        //Amen.
+    }
+}
+        
+        
+        
+/* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
